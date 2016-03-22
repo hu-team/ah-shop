17f0e7595e2eed8510ed36daf1e8017c55c5f6a2
Thierry Belliot <thierry.belliot@student.hu.nl>
2016-03-22 22:04:42 +0100

    find products

---
+++ src/resources/product.js
@@ -0,0 +1,44 @@
+/**
+ * Sample Resource, will contain route definitions and logic for saving/fetching data.
+ */
+'use strict';
+
+import Resource from "../resource";
+
+/**
+ * Sample Resource
+ * @class Sample
+ * @type {Sample}
+ */
+export default class Sample extends Resource {
+  constructor (server) {
+    super(server);
+  }
+
+  /**
+   * Define Routes.
+   */
+  routes () {
+    var Product = this.models.product;
+
+    this.server.get('/products', function(req, res, next) {
+      Product.findAll().then(function(product){
+        //var data = product.getDataValue();
+
+        var msg = {
+          type: "data",
+          data: product,
+          count: product.length
+        }
+
+        res.json(msg);
+        return next();
+      });
+    });
+  }
+
+  getRoot (req, res, next) {
+    res.end('<h1>Webservice, AH - Shop!</h1>');
+    return next();
+  }
+}

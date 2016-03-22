385c8b5c3ad95e3438fa9500858b5333ab373ed9
Thierry Belliot <thierry.belliot@student.hu.nl>
2016-03-22 20:51:08 +0100

    create user resource.

---
+++ src/resources/user.js
@@ -0,0 +1,97 @@
+/**
+ * Sample Resource, will contain route definitions and logic for saving/fetching data.
+ */
+'use strict';
+
+import Resource from "../resource";
+
+/**
+ * Sample Resource
+ * @class Sample
+ * @type {Sample}
+ */
+export default class UserRoute extends Resource {
+
+  constructor (server) {
+    super(server);
+  }
+
+  /**
+   * Define Routes.
+   */
+  routes () {
+    var User = this.models.user;
+    var UserInfo = this.models.user_info;
+    var self = this;
+
+    this.server.post('/users', function(req, res, next) {
+      var username = req.body.username;
+      var firstname = req.body.firstname;
+      var lastname = req.body.lastname;
+      var password = self.generatePassword();
+
+      //console.log(req.body.username);
+
+      User.create({
+        username: username,
+        password: password
+      }).then(function(user) {
+        var id = user.get({
+          plain: true
+        })['id'];
+
+        UserInfo.create({
+          firstname: firstname,
+          lastname: lastname,
+          balance: 20.78,
+          userId: id
+        }).then(function(){
+          var msg = {
+            type: "CREATED",
+            message: `A user account was created, this is your password ${password}`
+          };
+          self.Response(res, next, msg);
+        });
+
+      }).catch(function(err) {
+        var msg = {
+          type: "ERROR",
+          message: err.errors[0]['message']
+        }
+        self.errResponse(res, next, msg);
+      });
+    });
+  }
+
+  Response(res, next, msg) {
+    res.json(msg);
+    return next();
+  }
+
+  generatePassword() {
+    var length = 8,
+    charset = "abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    retVal = "";
+
+    for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    return retVal;
+  }
+
+  errResponse(res, next, msg) {
+
+    res.json(msg);
+    return next();
+  }
+
+  setUser (req, res, next) {
+    var username = req.body.username;
+    var firstname = req.body.firstname;
+    var lastname = req.body.lastname;
+
+    //res.end();
+    return next();
+  }
+}

27d14b21515ee90c1393e541efc62052fecf32b6
Tom Valk <tomvalk@gmail.com>
2016-05-18 20:22:47 +0200

    Adding target (build output) to ignore file

--- wsinterface/target/generated-sources/wsimport/nl/arvici/ahshop/wsinterface/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://schemas.arvici.nl/messages/response", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package nl.arvici.ahshop.wsinterface;

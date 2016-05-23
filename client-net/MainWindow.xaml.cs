f3e2a3ddb762c99ae94461fee2238f8cd872c4d7
Tom Valk <tomvalk@gmail.com>
2016-05-23 14:38:41 +0200

    Adding .net gui client stub.

---
+++ client-net/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace client_net
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            string username = textBox1.Text;
+            string firstname = textBox2.Text;
+            string lastname = textBox3.Text;
+
+            MessageBox.Show(" " + "", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            MessageBox.Show(" " + "", "Account Created!", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string username = textBox.Text;
+            string password = passwordBox.Password.ToString();
+
+            MessageBox.Show(" " + "", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            UserDashboard userdashboard = new UserDashboard(loginuser);
+            userdashboard.Show();
+            this.Close();
+        }
+    }
+}

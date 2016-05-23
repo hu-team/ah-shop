c56dfaaa25833b3c35424e2c94919a178ccb5cd9
Tom Valk <tomvalk@gmail.com>
2016-05-23 15:41:19 +0200

    Adding generic response types.

---
+++ server/ShopServiceUtil.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace server
+{
+    [DataContract]
+    public class ServiceResponse<T>
+    {
+        [DataMember]
+        public T Data;
+        [DataMember]
+        public MetaData Meta;
+
+        public ServiceResponse (T data, MetaData meta)
+        {
+            Data = data;
+            Meta = meta;
+        }
+
+        public ServiceResponse(MetaData meta)
+        {
+            Meta = meta;
+        }
+    }
+
+
+    [DataContract]
+    public class MetaData
+    {
+        [DataMember]
+        public bool Success;
+        [DataMember]
+        public string Message;
+
+        public MetaData(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+
+        public MetaData(bool success)
+        {
+            Success = success;
+            Message = "";
+        }
+    }
+}
